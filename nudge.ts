5dd2e1ad613a6d2741419291cbe464ccbdd35e8d
Ryan Hatfield <rhatfield@bushelpowered.com>
2022-03-21 03:42:53 -0500

    Change nudge cmd args

--- nudge.ts
+++ nudge.ts
@@ -4,14 +4,13 @@ import { serve } from "https://deno.land/std@0.127.0/http/server.ts";
 const args = parse(Deno.args, {
   default: {
     port: 4325,
-    cmd: "echo",
-    args: "nudge",
+    cmd: `["uname","-sm"]`,
     status: 204,
   },
 });
 
 const port = args.port as number;
-const cmd = [args.cmd as string, args.args];
+const cmd = JSON.parse(args.cmd);
 const status = args.status as number;
 
 await serve(() => {
